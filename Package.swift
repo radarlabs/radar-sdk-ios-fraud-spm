8611b1e4bf6459d42179e8829431d2d4bad3aada
lmeier <10292336+lmeier@users.noreply.github.com>
2026-02-11 14:16:04 +0000

    [create-pull-request] automated change

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
         .target(name: "_RadarStubFraud"),
         .binaryTarget(
             name: "RadarSDKFraud",
-            url: "https://github.com/radarlabs/radar-sdk-ios-fraud-spm/releases/download/0.0.2-beta.7/RadarSDKFraud.xcframework.zip",
-            checksum: "3f6e7896350fd6c5a57ad1fc8c944b45aeffde912058a7ca35c30bbfa743e6e3"
+            url: "https://github.com/radarlabs/radar-sdk-ios-fraud-spm/releases/download/0.0.2-beta.8/RadarSDKFraud.xcframework.zip",
+            checksum: "cea9807ce98b0008835dc93b1d40c65a61ef6b9278ec2fc670534f28c240b8a8"
         )
     ]
 )
